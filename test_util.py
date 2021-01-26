1471d6837ecb0a82a5cbfc46644d30ae012a0b18
Adrian Rodriguez <Rodriguez.Adrian609@gmail.com>
2021-01-25 20:00:04 -0500

    refactorin python test into a test class

--- test_util.py
+++ test_util.py
@@ -1,5 +1,24 @@
 import util
 
-def test_util():
-    result = util.str_to_int('1.99')
-    assert result == 1
+
+class TestFloats:
+
+    def setup(self):
+        print("\nthis is setup")
+
+    def teardown(self):
+        print("\nthis is teardown")
+
+    def setup_class(cls):
+        print("\nthis is teardown class")
+
+    def teardown_class(cls):
+        print("\nthis is teardown class")
+
+    def test_float_rounds_down(self):
+        result = util.str_to_int('1.99')
+        assert result == 1
+
+    def test_float_round_down_lesser_half(self):
+        result = util.str_to_int('1.2')
+        assert result == 1
