f9385bf71583cbc99237fe738b9ec93bdf5a086f
Adrian Rodriguez <Rodriguez.Adrian609@gmail.com>
2021-01-25 19:45:23 -0500

    adding python test skeleton

---
+++ test_util.py
@@ -0,0 +1,5 @@
+import util
+
+def test_util():
+    result = util.str_to_int('1.99')
+    assert result == 1
---
+++ util.py
@@ -0,0 +1,23 @@
+from math import floor
+
+def str_to_int(string, round_down=True):
+    """
+    parses a string number into an integer, optionally converting to a float
+    and rounding down.
+    """
+    error_msg = "Unable to convert to integer '%s'" % str(string)
+    try:
+        integer = float(string.replace(',', '.'))
+    except AttributeError:
+        if isinstance(string, (int, float)):
+            integer = string 
+        else:
+            raise RuntimeError(error_msg)
+    except (TypeError, ValueError):
+        raise RuntimeError(error_msg)
+
+    if round_down:
+        integer = floor(integer)
+    else:
+        integer = round(integer)
+    return int(integer)
